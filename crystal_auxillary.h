b2bad633fb3c6092d98f0bd2aaabd3d0cdbacb78
MainframeKnight <abramov.chess@gmail.com>
2024-12-04 20:27:02 +0300

    Added ranged random number generation.

--- crystal_auxillary.h
+++ crystal_auxillary.h
@@ -4,16 +4,27 @@
 #include<stdlib.h>
 #include<time.h>
 
-int random_int() {
-    int num;
-    getentropy(&num, sizeof(int));
+unsigned int random_uint() {
+    unsigned int num;
+    getentropy(&num, sizeof(unsigned int));
     return num;
 }
 
-long random_long() {
-    long num;
-    getentropy(&num, sizeof(long));
+unsigned long random_ulong() {
+    unsigned long num;
+    getentropy(&num, sizeof(unsigned long));
     return num;
+}
+
+unsigned int random_unbiased_uint(unsigned int a, unsigned int b) {
+    int rnd;
+    while (1) {
+        rnd = random_uint();
+        if (rnd < 4294967295/(b - a) * (b - a)) {
+            break;
+        }
+    }
+    return a + rnd % (b - a);
 }
 
 char* get_datetime() {
